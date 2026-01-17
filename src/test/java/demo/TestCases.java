4a35e0240d4021555e15a99991c742e9ad148503
Mahantakumarakshay1995 <86707104+Mahantakumarakshay1995@users.noreply.github.com>
2026-01-18 00:40:18 +0530

    ME_QA_XFLIPKART_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -9,13 +9,18 @@ import org.openqa.selenium.logging.LoggingPreferences;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebElement;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 
 
-// import io.github.bonigarcia.wdm.WebDriverManager;
+
+
 import demo.wrappers.Wrappers;
 
 public class TestCases {
@@ -25,6 +30,91 @@ public class TestCases {
      * TODO: Write your tests here with testng @Test annotation. 
      * Follow `testCase01` `testCase02`... format or what is provided in instructions
      */
+    @Test
+    public void testCase01() throws InterruptedException {
+       
+        driver.get("https://www.flipkart.com/");
+        WebElement search =driver.findElement(By.xpath("//input[@name='q']"));
+        Wrappers.findSearchAndClick(search,"Washing Machine");
+        Thread.sleep(2000);
+        System.out.println("Wait1");
+
+        WebElement popularelement = driver.findElement(By.xpath("//div[contains(text(),'Popularity')]"));
+        popularelement.click();
+
+        Thread.sleep(3000);
+
+        List<WebElement> listOfRatings=driver.findElements(By.xpath("//div[@class='MKiFS6']//parent::span"));
+        int  count=Wrappers.getAllRatingUnderNumber(listOfRatings,4.0);
+        System.out.println("Number of count below 4 is:"+count);
+
+
+    }
+
+     @Test
+    public void testCase02() throws InterruptedException {
+        driver.get("https://www.flipkart.com/");
+        WebElement search =driver.findElement(By.xpath("//input[@name='q']"));
+        Wrappers.findSearchAndClick(search,"iPhone");
+        Thread.sleep(2000);
+        System.out.println("Wait1");
+
+       WebElement discount =driver.findElement(By.xpath("//div[contains(text(),'10% or more')]"));
+        Wrappers.clickOnFilter(discount);
+
+        Thread.sleep(1000);
+        System.out.println("Wait2");
+
+
+        List<WebElement> offelements=driver.findElements(By.xpath("//div[@class='HQe8jr']//span"));
+        Wrappers.getAllDiscountabove(offelements, 17,driver);
+
+    }
+
+    @Test
+    public void testCase03() throws InterruptedException {
+        driver.get("https://www.flipkart.com/");
+        WebElement search =driver.findElement(By.xpath("//input[@name='q']"));
+        Wrappers.findSearchAndClick(search,"Coffee Mug");
+        Thread.sleep(5000);
+        System.out.println("Wait1");
+ 
+        WebElement ratingButton =driver.findElement(By.xpath("//div[contains(text(),'4') and contains(text(),'above')]"));
+        Wrappers.waitTillClicakable(ratingButton,driver);
+        Wrappers.clickOnFilter(ratingButton);
+        Thread.sleep(2000);
+        System.out.println("Wait2");
+
+        List<WebElement> titleselement=driver.findElements(By.xpath("(//div[contains(@class,'dLgFEE')])[3]//a[contains(@class,'pIpigb')]"));
+        List<String> title=new ArrayList<>();
+        for(int i=0;i< 5;i++){
+            String text =titleselement.get(i).getText();
+            title.add(text);
+            System.out.println("Title is : " + (i+1) + ":" + text);
+        }
+
+         List<WebElement> imageurlelement=driver.findElements(By.xpath("(//div[contains(@class,'dLgFEE')])[3]//a[contains(@class,'GnxRXv')]"));
+        List<String> image=new ArrayList<>();
+        for(int i=0;i< 5;i++){
+            WebElement element =imageurlelement.get(i);
+            String hrefvalue = element.getAttribute("href");
+            //image.add(text1);
+            System.out.println("Image URL is : " + (i+1) + ":" + hrefvalue);
+        }
+
+        
+         List<WebElement> ratingurlelement=driver.findElements(By.xpath("(//div[contains(@class,'dLgFEE')])[3]//span[contains(@class,'PvbNMB')]"));
+        List<String> rating=new ArrayList<>();
+        for(int i=0;i< 5;i++){
+            WebElement ratingelement =ratingurlelement.get(i);
+            String ratingtext = ratingelement.getText();
+            //ratingtext=ratingtext.split(ratingtext)
+            rating.add(ratingtext);
+            System.out.println("Rating  is : " + (i+1) + ":" + ratingtext);
+        }
+        
+
+    }
 
      
     /*
@@ -35,8 +125,7 @@ public class TestCases {
     {
         System.setProperty("java.util.logging.config.file", "logging.properties");
 
-        // NOT NEEDED FOR SELENIUM MANAGER
-        // WebDriverManager.chromedriver().timeout(30).setup();
+       
 
         ChromeOptions options = new ChromeOptions();
         LoggingPreferences logs = new LoggingPreferences();
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -7,16 +7,88 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.*;
+import org.openqa.selenium.JavascriptExecutor;
 
 public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
-}
+    public static void findSearchAndClick(WebElement element,String text){
+        try {
+            element.clear();
+            element.sendKeys(text);
+            element.submit();
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+        
+    }
+    public static void scrollAndClick(WebElement element, ChromeDriver driver){
+        try{
+        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);",element);
+        element.click();
+        //element.submit();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    public static int getAllRatingUnderNumber(List<WebElement> allele, Double n){
+        int count=0;
+        for(int i=0;i< allele.size();i++){
+
+                    
+            if(Double.parseDouble(allele.get(i).getText()) <=  n){
+             count++;
+            }
+        }
+            return count;
+        }
+
+        public static void clickOnFilter(WebElement element){
+            element.click();
+        } 
+        public static void waitTillClicakable(WebElement element,ChromeDriver driver){
+            WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(20));
+            wait.until(ExpectedConditions.elementToBeClickable(element));
+            System.out.println("element located");
+        }
+
+        public static void getAllDiscountabove(List<WebElement> ele, int n, ChromeDriver driver){
+            //int value=Integer.parseInt(s.split("%")[0].trim());
+            int count=0;
+            int count1=0;
+            List<WebElement> titleslist =driver.findElements(By.xpath("//div[@class='HQe8jr']//ancestor::div[@class='ZFwe0M row']//child::div[@class='RG5Slk']"));
+            for(int i=0;i<ele.size();i++){
+                String titletext=titleslist.get(i).getText();
+               String discounttext=ele.get(i).getText();
+
+               //System.out.println("before trim: "+discounttext);
+               int discountnumber=Integer.parseInt(discounttext.replaceAll("\\D+", ""));
+               System.out.println("After trim: "+discountnumber);
+
+               if(discountnumber >n){
+                System.out.println("Inside if loop Titale name: "+titletext);
+                System.out.println("Inside if loop Discount Number: "+discountnumber+" %");
+                 System.out.println("--------------------------------");
+                count1++;
+
+               }
+
+               count++;
+            }
+            System.out.println("Number of Total element are :"+count);
+            System.out.println("Number of element Who are more than 17 percent discounted :"+count1);
+           
+
+        }
+    }
+
